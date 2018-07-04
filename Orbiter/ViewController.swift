0e794d06712fbe6a0cebfb58223f6a890a62d4af
Charlie Griffin <charliegriffin@email.arizona.edu>
2018-07-04 14:30:20 -0500

    create basic 1d Euler method integrator

--- Orbiter/ViewController.swift
+++ Orbiter/ViewController.swift
@@ -9,17 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var position : Double = 0
+    var velocity : Double = 1
+    var force : Double = 1
+    var mass : Double = 1
+    var timeStep : Double = 0.1
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        print("started")
+        print("initial values")
+        print("position \(position)")
+        print("velocity \(velocity)")
+        for _ in 1...10 {
+            findNextPosition()
+            print("position \(position)")
+            print("velocity \(velocity)")
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func findNextPosition() {
+        // Euler algorithm
+        // TODO: Connect with the particle object
+        // TODO: Improve to Leapfrog integration
+        position = position + velocity*timeStep
+        velocity = velocity + force/mass*timeStep
+    }
 
 }
 
